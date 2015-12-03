8769efc2b20670e32dcce35cf67288f3dfd79e0b
“pmlc” <pmlc@iinet.net.au>
2015-12-03 01:41:01 +0000

    stylesheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   
-  resources :articles do
-    resources :comments
-  end
- 
-  root 'welcome#index'
+  root 'pages#home'
   
+  get '/home', to: 'pages#home'
+ 
 end
