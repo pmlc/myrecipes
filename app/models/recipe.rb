036fa1b890ea6e2ff764ca1ef7c33530bab94a11
“pmlc” <pmlc@iinet.net.au>
2015-12-08 10:21:56 +0000

    Models updated and first use of Testing

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,10 @@
 class Recipe < ActiveRecord::Base
 
+  belongs_to :chef
+
+  validates :name, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :summary, presence: true
+  validates :description, presence: true
+
+
 end
---
+++ test/models/recipe_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+  
+  #def setup
+  #  @recipe = Recipe.new(name:"chicken parm", 
+  #            summary: "this is the best chickenever",
+  #            description: "heat oil, add onions, add tomato sauce,cook for 20 minutes")
+  #end
+ 
+end
+  
