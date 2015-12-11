0052904dff4815c12a30407620e3cac1acc120d6
“pmlc” <pmlc@iinet.net.au>
2015-12-11 01:40:26 +0000

    Recipes routes, pages added

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,26 @@
+class RecipesController < ApplicationController
+  
+  def index
+    @recipes = Recipe.all
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+    # binding.pry
+    @recipe = Recipe.find(params[:id]) 
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  
+
   root 'pages#home'
-  
   get '/home', to: 'pages#home'
+  
+  resources :recipes
+  resources :chefs
  
 end
