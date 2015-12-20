b210ea5b9c2a60e4534d1e9712052a626add750c
“pmlc” <pmlc@iinet.net.au>
2015-12-20 23:38:59 +0000

    Navigation and Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,22 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  # ading thses helper methods means can use these in screens - and not just controllers
+  helper_method :current_user, :logged_in?
+  
+  def current_user
+    current_user ||= Chef.find(session[:chef_id]) if session[:chef_id]
+  end
+  
   def logged_in?
-    ### !!current_user
-    true
+    !!current_user
+  end
+  
+  def require_same_user
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perform that action"
+      redirect_to recipes_path
+    end
   end
   
 end
--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -7,8 +7,23 @@ class ChefsController < ApplicationController
     @chefs = Chef.paginate(page: params[:page], per_page: 4)
   end
 
+  def new
+    @chef = Chef.new
+  end
+
+def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      flash[:success] = "Your account has been created succesfully"
+      session[:chef_id] = @chef.id
+      redirect_to recipes_path
+    else
+      render 'new'
+    end
+  end
+
   def edit
-    @chef = Chef.find(params[:id])
+
   end
 
   def update
@@ -22,15 +37,9 @@ class ChefsController < ApplicationController
   end
 
   def show
-    ##binding.pry   / params
-    @chef = Chef.find(params[:id]) 
-    
     @recipes = @chef.recipes.paginate(page: params[:page], per_page: 3)
   end
 
-  def destroy
-  end
-  
   private
 
     def chef_params
